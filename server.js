e8fe1518811593febb9e3153c148840ecd5f0cfa
Arimathea-Suarez <ahsuarez@up.edu.ph>
2025-03-19 12:14:16 +0800

    Added comments on server.js

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 // Arimathea Charmille H. Suarez
 // CMSC 100 - U3L
 
+// To import the express
 const express = require('express');
 const path = require('path');
 const app = express();
 
-
+// Static files
 app.use(express.static('files'));
 
+// Starts the server
 const PORT = 3000;
 app.listen(PORT, () => {
     console.log(`Server running at http://localhost:${PORT}`);
